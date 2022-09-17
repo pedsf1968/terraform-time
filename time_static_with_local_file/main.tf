1e5a14be20c22f6cf341db02bd40de298a6f0b95
terraform <terraform@isa-server>
2022-09-17 18:14:42 +0200

    centrilise provider plugin directory

--- time_static_with_local_file/main.tf
+++ time_static_with_local_file/main.tf
@@ -1,11 +1,10 @@
 # Simple time_static resource
-# change the filename with your own path
 
 resource "time_static" "time_update" {
 }
 
 resource "local_file" "time" {
-  filename = "/home/terraform/data/time/time_static_with_local_file/time.txt"
+  filename = "${path.module}/time.txt"
   content = "Time stamp of this file is ${time_static.time_update.id}"
 }
 
